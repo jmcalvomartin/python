768ac53b6df743070792b68974546a8ba4d75a9b
Jorge Calvo <jorge@europeanvalley.es>
2021-01-05 20:16:05 +0100

    Create 006_Dictionary.py

---
+++ Scripts/006_Dictionary.py
@@ -0,0 +1,13 @@
+dicc = {'Nombre' : 'Arnaldo', 'Edad' : 28}
+
+print(dicc['Nombre'], dicc['Edad'])
+
+
+
+dicc['Nombre'] = 'Gretica'
+dicc['Edad'] = 27
+print(dicc['Nombre'], dicc['Edad'])
+
+
+
+print('Nombre' in dicc)
