ce90a23a4a7849554f5cbc13702309ff3720ec43
Jorge Calvo <jorge@europeanvalley.es>
2021-01-05 20:16:03 +0100

    Create 011_CiclosPython.py

---
+++ Scripts/011_CiclosPython.py
@@ -0,0 +1,10 @@
+# ejemplo de ciclo while desarrollado en Python
+i = 1
+while i <= 5:
+        print("Iteracion", i)
+        i += 1
+
+# ejemplo de ciclo for con range
+print("Yo me llamo")
+for i in range(1,6,2):
+        print("Emilio el de las 5 (" + str(i) + ")")
